5585e96eb441d437f5fb578c4f8014626b9a70c5
Romy Lynch <romylynch@gmail.com>
2019-12-10 16:57:43 +0000

    Finished up - default state not on

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,28 @@
+class TasksController < ApplicationController
+
+    def index
+        @tasks = Task.all
+    end
+
+    def show
+        @task = Task.find(params[:id])
+    end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        task = Task.new(task_params)
+        task.save
+        redirect_to tasks_path
+    end
+
+
+    private
+    def task_params
+        params.require(:task).permit(:title, :details, :completed)
+    end
+   
+
+end
